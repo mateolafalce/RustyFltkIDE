3724fe2f2583590544c636ac5aaf9a50b878e8ec
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-07 02:04:10 +1100

    mannage path exist error

--- src/functions/get_root.rs
+++ src/functions/get_root.rs
@@ -1,24 +1,34 @@
 use std::{
-    fs::File,
+    fs::{
+        File,
+        OpenOptions
+    },
     io::{
         Read,
         Write
     },
-    env
+    env,
+    path::Path,
 };
 
 pub fn root() -> String {
-    let mut file: File = File::open("src/constants/root.rs").expect("Error");
-    let mut contents: String = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => {
-            contents.push_str(r"\🎯");
-            return contents
-        },
-        Err(_) => {
-            file.write_all(env::current_dir().unwrap().display().to_string().as_bytes()).unwrap();
-            contents.push_str(r"\🎯");
-            return contents
-        }
+    let mut file: File = File::open("src/constants/root.rs").unwrap();
+    let mut path: String = String::new();
+    file.read_to_string(&mut path).unwrap();
+    if Path::new(&path).exists() {
+        path.push_str(r"\🎯");
+        return path
+    } else {
+        let mut path: String = String::new();
+        let mut root_dir: File = OpenOptions::new()
+        .write(true)
+        .create(false)
+        .truncate(true)
+        .open("src/constants/root.rs")
+        .unwrap();
+        root_dir.write_all(env::current_dir().unwrap().display().to_string().as_bytes()).unwrap();
+        root_dir.read_to_string(&mut path).unwrap();
+        path.push_str(r"\🎯");
+        return path
     }
 }
