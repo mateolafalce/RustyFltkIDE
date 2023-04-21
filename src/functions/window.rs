a89e752dd9be5b96bd534090278d3c6c957887bd
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-22 00:37:23 +1100

    text_editor.rs comments

--- src/functions/window.rs
+++ src/functions/window.rs
@@ -9,18 +9,11 @@ use std::path::Path;
 
 // Define a function called "window" that returns a Window object.
 pub fn window() -> Window {
-    // Load an icon from a file and assign it to a variable called "icon".
-    let icon: IcoImage = IcoImage::load(&Path::new("src/rusty.ico")).unwrap();
-    // Create a new window with the specified size and title.
+    let icon: IcoImage = IcoImage::load(&Path::new("src/rusty.ico")).unwrap(); // Load an icon
     let mut window: Window = Window::new(100, 100, 1000, 600, "🪂 Rusty IDE 🪂");
-    // Set the window's icon to the icon we loaded earlier.
-    window.set_icon(Some(icon));
-    // Enable the window's border.
-    window.set_border(true);
-    // Allow the user to resize the window.
-    window.make_resizable(true);
-    // Set the window's background color to white.
-    window.set_color(Color::White);
-    // Return the window object.
-    window
+    window.set_icon(Some(icon)); // Set the window's icon to the icon we loaded earlier.
+    window.set_border(true); // Enable the window's border.
+    window.make_resizable(true); // Allow the user to resize the window.
+    window.set_color(Color::White); // Set the window's background color to white.
+    window // Return the window object.
 }
