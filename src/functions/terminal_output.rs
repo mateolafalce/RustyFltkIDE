19ff632f72308068cba42ffa0639cc2fbeb496cd
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-23 02:23:04 +1100

    terminal_output.rs

--- src/functions/terminal_output.rs
+++ src/functions/terminal_output.rs
@@ -21,13 +21,13 @@ pub fn terminal_output() -> (TextDisplay, TextBuffer) {
     let terminal_buffer: TextBuffer = TextBuffer::default();
     let mut terminal: TextDisplay = TextDisplay::new(200, 400, 790, 170, None);
     let terminal_text: String = format!("Operating system: {}\n🦀 Rusty IDE console 💻\n\n", OS);
-    terminal.set_buffer(Some(terminal_buffer.clone()));
-    terminal.buffer().unwrap().append(&terminal_text);
-    terminal.set_scrollbar_align(Align::Bottom | Align::Right);
+    terminal.set_buffer(Some(terminal_buffer.clone())); // Set the text buffer for the text display widget
+    terminal.buffer().unwrap().append(&terminal_text); // Add the welcome message to the text buffer
+    terminal.set_scrollbar_align(Align::Bottom | Align::Right); // Set the scrollbar alignment to bottom-right
     terminal.set_text_font(FONT);
     terminal.set_text_size(16);
     terminal.set_text_size(CONSOLE_TEXT_SIZE);
     terminal.set_scrollbar_size(18);
-    terminal.set_frame(FrameType::NoBox);
-    (terminal, terminal_buffer)
+    terminal.set_frame(FrameType::NoBox); // Set the frame type to no box (no visible border)
+    (terminal, terminal_buffer) // Return a tuple containing the text display widget and text buffer widget
 }
