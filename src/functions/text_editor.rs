9a4cd7d61cc3b9ca353a627f2dd463f578aac152
Mateo Lafalce <mateolafalce3@gmail.com>
2023-04-23 02:31:05 +1100

    fix comments

--- src/functions/text_editor.rs
+++ src/functions/text_editor.rs
@@ -5,28 +5,24 @@ use fltk::{
         Color,
     },
     text::{
-        TextEditor, 
+        TextEditor,
         TextBuffer,
         Cursor,
     }
 };
 
-// Function to create a new text editor with a text buffer
 pub fn text_editor() -> (TextEditor, TextBuffer) {
-    // Create a new text buffer
     let mut buffer: TextBuffer = TextBuffer::default();
     buffer.set_tab_distance(4); // Set the tab distance to 4 spaces
-    // Create a new text editor
-    let mut text_editor: TextEditor = TextEditor::new(200, 20, 790, 380, ""); // X position, Y position, Width, Height, Label
-    text_editor.set_text_font(Font::Courier); // Set the text font to Courier
-    text_editor.set_text_size(17); // Set the text size to 17
-    text_editor.set_scrollbar_size(16); // Set the size of the scrollbar
+    let mut text_editor: TextEditor = TextEditor::new(200, 20, 790, 380, None);
+    text_editor.set_text_font(Font::Courier);
+    text_editor.set_text_size(17);
+    text_editor.set_scrollbar_size(16);
     text_editor.set_buffer(Some(buffer.clone())); // Set the text buffer for the text editor
     text_editor.show_cursor(true); // Show the cursor in the text editor
     text_editor.set_linenumber_width(25); // Set the width of the line numbers
     text_editor.set_linenumber_fgcolor(Color::from_hex_str("#000000").unwrap()); // Set the color of the line numbers
     text_editor.set_linenumber_bgcolor(Color::from_hex_str("#D3D3D3").unwrap()); // Set the background color of the line numbers
-    text_editor.set_cursor_style(Cursor::Simple); // Set the cursor style to a simple style
-    // Return the text editor and the text buffer as a tuple
-    (text_editor, buffer)
+    text_editor.set_cursor_style(Cursor::Simple);
+    (text_editor, buffer) // Return the text editor and the text buffer as a tuple
 }
--- src/functions/window.rs
+++ src/functions/window.rs
@@ -1,4 +1,3 @@
-// Import the necessary modules from the fltk crate and the std library.
 use fltk::{
     prelude::*,
     window::Window,
@@ -7,9 +6,8 @@ use fltk::{
 };
 use std::path::Path;
 
-// Define a function called "window" that returns a Window object.
 pub fn window() -> Window {
-    let icon: IcoImage = IcoImage::load(&Path::new("src/rusty.ico")).unwrap(); // Load an icon
+    let icon: IcoImage = IcoImage::load(&Path::new("src/rusty.ico")).unwrap();
     let mut window: Window = Window::new(100, 100, 1000, 600, "🪂 Rusty IDE 🪂");
     window.set_icon(Some(icon)); // Set the window's icon to the icon we loaded earlier.
     window.set_border(true); // Enable the window's border.
