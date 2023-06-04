1988b5c2813b3664e4ee95301828187deb1940f5
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-05 01:09:32 +1100

    mouse_select.rs

--- src/functions/event/mouse_select.rs
+++ src/functions/event/mouse_select.rs
@@ -1,10 +1,12 @@
 pub fn mouse_select(event: fltk::enums::Event) -> bool {
     match event {
         fltk::enums::Event::Enter => {
+            // Set the cursor to a hand when the mouse enters
             fltk::draw::set_cursor(fltk::enums::Cursor::Hand);
             true
         },
         fltk::enums::Event::Leave => {
+            // Set the cursor to an arrow when the mouse leaves
             fltk::draw::set_cursor(fltk::enums::Cursor::Arrow);
             true
         },
