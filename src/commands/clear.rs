3ad078f87664700a3849ab6c2b48a0fd38f3441f
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-05 01:06:14 +1100

    clear.rs

--- src/commands/clear.rs
+++ src/commands/clear.rs
@@ -1,28 +1,17 @@
-use std::thread;
-use fltk::{
-    prelude::*,
-    text::{
-        TextDisplay,
-        TextBuffer
-    }
-};
+use fltk::prelude::DisplayExt;
 #[path="../functions/write_terminal.rs"]
 mod write_terminal;
 
 pub fn clear(
-    text: TextBuffer,
-    terminal: TextDisplay,
+    text: fltk::text::TextBuffer,
+    terminal: fltk::text::TextDisplay,
 ) {
-    thread::spawn(move || {
-        let mut text: TextBuffer = text.clone();
+    std::thread::spawn(move || {
+        let mut text: fltk::text::TextBuffer = text.clone();
         text.set_text("");
-        let mut terminal: TextDisplay = terminal.clone();
+        let mut terminal: fltk::text::TextDisplay = terminal.clone();
         terminal.set_buffer(Some(text.clone()));
         terminal.buffer().unwrap().append("");
-        write_terminal::write_terminal(
-            "\n",
-            text,
-            terminal
-        ).expect("Error");
+        write_terminal::write_terminal("\n",text,terminal).expect("Error");
     });
 }
