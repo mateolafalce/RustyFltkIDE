ebacb3fdd0a156e436570cb388795e8d016c9f3b
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-10 02:59:00 +1100

    cargo_build

--- src/commands/cargo_build.rs
+++ src/commands/cargo_build.rs
@@ -1,17 +1,21 @@
 #[path="../functions/event/command_result.rs"]
 mod command_result;
 
+// This function builds a Cargo project asynchronously
 pub fn cargo_build(
     input: String,
     text: fltk::text::TextBuffer,
     terminal: fltk::text::TextDisplay,
     root: String
 ) {
+    // Spawn a new thread to run the build command
     std::thread::spawn(move || {
+        // Execute the "cargo build" command with the given manifest path
         let output: std::process::Output = std::process::Command::new("cargo")
             .args(&["build", "--manifest-path", &((root + "\\Cargo.toml"))])
             .output()
             .expect("Error");
+        // Update console
         command_result::command_result(output, input, text, terminal);
     });
 }
