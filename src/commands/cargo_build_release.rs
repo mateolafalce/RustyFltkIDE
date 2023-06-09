a3387eed6cdc7f632c6c4f6129ef0c2821ade43a
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-10 02:57:41 +1100

    cargo_build_release

--- src/commands/cargo_build_release.rs
+++ src/commands/cargo_build_release.rs
@@ -1,17 +1,21 @@
 #[path="../functions/event/command_result.rs"]
 mod command_result;
 
+// This function builds a Cargo project in release mode asynchronously
 pub fn cargo_build_release(
     input: String,
     text: fltk::text::TextBuffer,
     terminal: fltk::text::TextDisplay,
     root: String
 ){
+    // Spawn a new thread to run the build command
     std::thread::spawn(move || {
+        // Execute the "cargo build --release" command with the given manifest path
         let output: std::process::Output  = std::process::Command::new("cargo")
             .args(&["build", "--release", "--manifest-path", &((root + "\\Cargo.toml"))])
             .output()
             .expect("Error");
+        // Update Console
         command_result::command_result(output, input, text, terminal);
     });
 }
