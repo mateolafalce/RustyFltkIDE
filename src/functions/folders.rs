2aa3cf93107f3ebe2a4bf6c8f1f7c72827c3e6bb
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-11 01:24:26 +1100

    render_tree_when_exist.rs

--- src/functions/folders.rs
+++ src/functions/folders.rs
@@ -1,53 +1,20 @@
-use fltk::{
-    prelude::*,
-    tree::{
-        Tree,
-        TreeSelect,
-        TreeConnectorStyle,
-        TreeReason,
-    },
-    enums::{
-        Color,
-    },
-};
+use fltk::prelude::*;
+
 #[path="./root/get_folders_roots.rs"]
 mod get_folders_roots;
 #[path="./root/get_all_paths_in_directory.rs"]
 mod get_all_paths_in_directory;
+#[path="./folders_functions/redner_tree_when_exist.rs"]
+mod redner_tree_when_exist;
 
-use std::{
-    path::Path,
-};
-
-pub fn folders() -> (Tree, Vec<String>) {
-    // Initialize variables
-    let mut folders: Tree = Tree::new(0, 20, 200, 580, None); // Create a new tree
-    let mut prefix: Vec<String> = vec![]; // Create an empty vector for prefix paths
-    let mut close_tree: Vec<String> = vec![]; // Create an empty vector for close tree paths
-    let (raw_path, is_the_repository_clear): (Vec<String>, bool) = get_folders_roots::get_folders_roots(); // Get root directories and check if repository is clear
+pub fn folders() -> (fltk::tree::Tree, Vec<String>) {
+    let mut folders: fltk::tree::Tree = fltk::tree::Tree::new(0, 20, 200, 580, None);
+    let mut prefix: Vec<String> = vec![];
+    let mut close_tree: Vec<String> = vec![];
+    let (raw_path, is_the_repository_clear): (Vec<String>, bool) = get_folders_roots::get_folders_roots();
     // Check if there are multiple root directories
     if raw_path.len() > 1 {
-        for i in 0..raw_path.len() - 1 { // Iterate over root directories except the last one
-            let mut split_path: Vec<&str> = raw_path[i].as_str().split('\\').collect(); // Split the path and get the last element
-            close_tree.push(split_path.last().unwrap().to_string()); // Add the last element to the close_tree vector
-            split_path.pop(); // Remove the last element from the split_path vector
-            prefix.push(split_path.join("/")); // Join the split_path elements with "/" and add to the prefix vector
-            // Get all paths in the directory and iterate over them
-            let paths: Vec<String> = get_all_paths_in_directory::get_all_paths_in_directory(
-                &Path::new(&raw_path[i]),
-                prefix[i].clone(),
-                is_the_repository_clear
-            );
-            for path in &paths {
-                if !path.contains("target") {
-                    folders.add(&path); // Add the path to the tree if it doesn't contain "target"
-                }
-            }
-        }
-        // Close the tree for each close tree path
-        for i in 0..raw_path.len() - 1 {
-            let _ = folders.close(&close_tree[i], true);
-        }
+        redner_tree_when_exist::redner_tree_when_exist(folders.clone(), close_tree, raw_path, is_the_repository_clear, prefix.clone());
     } else { // If there is only one root directory
         for i in 0..raw_path.len() {
             // Split the path and get the last element
@@ -57,7 +24,7 @@ pub fn folders() -> (Tree, Vec<String>) {
             prefix.push(split_path.join("/")); // Join the split_path elements with "/" and add to the prefix vector
             // Get all paths in the directory and iterate over them
             let paths: Vec<String> = get_all_paths_in_directory::get_all_paths_in_directory(
-                &Path::new(&raw_path[i]),
+                &std::path::Path::new(&raw_path[i]),
                 prefix[i].clone(),
                 is_the_repository_clear
             );
@@ -71,11 +38,10 @@ pub fn folders() -> (Tree, Vec<String>) {
         }
     }
     // Set tree properties
-    folders.set_callback_reason(TreeReason::Closed);
+    folders.set_callback_reason(fltk::tree::TreeReason::Closed);
     folders.set_show_root(false);
-    folders.set_select_mode(TreeSelect::Multi);
-    folders.set_connector_style(TreeConnectorStyle::Solid);
-    folders.set_connector_color(Color::from_rgb(100,100,0));
-    // Return the tree and prefix vectors
+    folders.set_select_mode(fltk::tree::TreeSelect::Multi);
+    folders.set_connector_style(fltk::tree::TreeConnectorStyle::Solid);
+    folders.set_connector_color(fltk::enums::Color::from_rgb(100,100,0));
     (folders, prefix)
 }
---
+++ src/functions/folders_functions/redner_tree_when_exist.rs
@@ -0,0 +1,33 @@
+#[path="../root/get_all_paths_in_directory.rs"]
+mod get_all_paths_in_directory;
+
+pub fn redner_tree_when_exist(
+    mut folders: fltk::tree::Tree,
+    mut close_tree: Vec<String>,
+    raw_path: Vec<String>,
+    is_the_repository_clear: bool,
+    mut prefix: Vec<String>,
+ ) -> bool {
+    for i in 0..raw_path.len() - 1 {
+        let mut split_path: Vec<&str> = raw_path[i].as_str().split('\\').collect(); // Split the path and get the last element
+        close_tree.push(split_path.last().unwrap().to_string()); // Add the last element to the close_tree vector
+        split_path.pop(); // Remove the last element from the split_path vector
+        prefix.push(split_path.join("/")); // Join the split_path elements with "/" and add to the prefix vector
+        // Get all paths in the directory and iterate over them
+        let paths: Vec<String> = get_all_paths_in_directory::get_all_paths_in_directory(
+            &std::path::Path::new(&raw_path[i]),
+            prefix[i].clone(),
+            is_the_repository_clear
+        );
+        for path in &paths {
+            if !path.contains("target") {
+                folders.add(&path); // Add the path to the tree if it doesn't contain "target"
+            }
+        }
+    }
+    // Close the tree for each close tree path
+    for i in 0..raw_path.len() - 1 {
+        let _ = folders.close(&close_tree[i], true);
+    }
+    true
+}
