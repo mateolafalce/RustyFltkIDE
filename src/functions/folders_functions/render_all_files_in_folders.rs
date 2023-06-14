214fa1a64a9e4feffcf5906765c7e502052b0200
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-15 00:53:26 +1100

    render_all_files_in_folders.rs

--- src/functions/folders_functions/render_all_files_in_folders.rs
+++ src/functions/folders_functions/render_all_files_in_folders.rs
@@ -1,18 +1,16 @@
-use fltk::{
-    tree::Tree,
-    text::TextBuffer,
-};
-
 #[path="../render_file.rs"]
 mod render_file;
 
 pub fn render_all_files_in_folders(
-    folders: Tree,
-    text_buffer: TextBuffer,
+    folders: fltk::tree::Tree,
+    text_buffer: fltk::text::TextBuffer,
     prefix: Vec<String>
 ) -> bool {
+    // Check if prefix has more than one element
     if prefix.len() > 0 {
+        // Iterate over the elements of prefix (excluding the last one)
         for i in 0..prefix.len() - 1 {
+            // Call render_file function for each prefix element
             render_file::render_file(
                 folders.clone(),
                 text_buffer.clone(),
@@ -20,8 +18,11 @@ pub fn render_all_files_in_folders(
             );
         }
     }
+    // Check if prefix has exactly one element
     if prefix.len() == 1 {
+        // Iterate over the single element of prefix
         for i in 0..prefix.len() {
+            // Call render_file function for the prefix element
             render_file::render_file(
                 folders.clone(),
                 text_buffer.clone(),
