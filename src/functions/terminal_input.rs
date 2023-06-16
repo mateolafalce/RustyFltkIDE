be76b354601657fc6a624ca3b6b08bfdc4598c22
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-17 00:47:22 +1100

    terminal_input.rs

--- src/functions/terminal_input.rs
+++ src/functions/terminal_input.rs
@@ -1,18 +1,4 @@
-use fltk::{
-    prelude::*,
-    text::{
-        TextDisplay,
-        TextBuffer
-    },
-    enums::{
-        FrameType,
-        Event,
-        Key
-    },
-    input::Input,
-    app::event_key
-};
-
+use fltk::prelude::*;
 #[path="../constants/text_size.rs"]
 mod text_size;
 #[path="../constants/font.rs"]
@@ -23,36 +9,36 @@ mod get_root;
 mod run_a_command;
 
 pub fn terminal_input(
-    terminal_output: TextDisplay,
-    terminal_buffer: TextBuffer,
+    terminal_output: fltk::text::TextDisplay,
+    terminal_buffer: fltk::text::TextBuffer,
 ) -> Input {
-    let mut terminal_input = Input::new(204, 570, 786, 30, None);
+    let mut terminal_input: fltk::input::Input = fltk::input::Input::new(204, 570, 786, 30, None);
     terminal_input.set_text_font(font::FONT);
     terminal_input.set_text_size(text_size::CONSOLE_TEXT_SIZE);
-    terminal_input.set_frame(FrameType::FlatBox);
+    terminal_input.set_frame(fltk::enums::FrameType::FlatBox);
     terminal_input.set_value(&get_root::get_root()); // Set the initial value for the input widget to a root directory
     terminal_input.set_readonly(true); // Set the input widget to be readonly by default
     let _terminal_input = terminal_output.clone();
     terminal_input.handle(move |terminal_input, event| {
         match event {
-            Event::KeyUp => { // Handle the key up event
+            fltk::enums::Event::KeyUp => { // Handle the key up event
                 if terminal_input.value().len() < get_root::get_root().len() {
                     terminal_input.set_value(&(terminal_input.value()));
                 }
                 true
             },
-            Event::Push => { // Handle the push event
+            fltk::enums::Event::Push => { // Handle the push event
                 terminal_input.set_readonly(false);
                 true
             },
-            Event::KeyDown => {
-                if event_key() == Key::Enter { // Check if the key pressed is the Enter key
+            fltk::enums::Event::KeyDown => {
+                if event_key() == fltk::enums::Key::Enter { // Check if the key pressed is the Enter key
                     run_a_command::run_a_command(terminal_input.value(),terminal_buffer.clone(),terminal_output.clone()).unwrap();
                     terminal_input.set_value(&get_root::get_root()); // Set the input widget value back to the root directory
                 }
                 true
             },
-            Event::Leave => { // Handle the leave event
+            fltk::enums::Event::Leave => { // Handle the leave event
                 terminal_input.set_readonly(true);
                 true
             },
