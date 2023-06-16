781aadc4d0fb92719972a6faf79a87c8b6a37f1b
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-17 00:51:06 +1100

    window.rs

--- src/functions/window.rs
+++ src/functions/window.rs
@@ -1,17 +1,11 @@
-use fltk::{
-    prelude::*,
-    window::Window,
-    image::IcoImage,
-    enums::Color
-};
-use std::path::Path;
+use fltk::prelude::*;
 
 pub fn window() -> Window {
-    let icon: IcoImage = IcoImage::load(&Path::new("src/rusty.ico")).unwrap();
-    let mut window: Window = Window::new(100, 100, 1000, 600, "🪂 Rusty IDE 🪂");
+    let icon: fltk::image::IcoImage = fltk::image::IcoImage::load(&std::path::Path::new("src/rusty.ico")).unwrap();
+    let mut window: fltk::window::Window = fltk::window::Window::new(100, 100, 1000, 600, "🪂 Rusty IDE 🪂");
     window.set_icon(Some(icon)); // Set the window's icon to the icon we loaded earlier.
     window.set_border(true); // Enable the window's border.
     window.make_resizable(true); // Allow the user to resize the window.
-    window.set_color(Color::White); // Set the window's background color to white.
+    window.set_color(fltk::enums::Color::White); // Set the window's background color to white.
     window // Return the window object.
 }
