f0f4939a0a14599e0abc770b4c043f6e8b9769fe
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-20 00:18:08 +1100

    error.rs

--- src/functions/event/error.rs
+++ src/functions/event/error.rs
@@ -2,10 +2,11 @@
 mod center;
 
 // Display an error message dialog at the center of the screen
-pub fn error(e: &str){
+pub fn error(e: &str) -> bool{
     fltk::dialog::alert(
         center::center().0 - 100,
         center::center().1 - 100,
         &format!("Error: {}", e)
     );
+    true
 }
