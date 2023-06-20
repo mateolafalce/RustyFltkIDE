ebac802ec4a226abde05302303c97f28057cb557
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-21 00:51:13 +1100

    terminal_output.rs scroll

--- src/functions/terminal_output.rs
+++ src/functions/terminal_output.rs
@@ -12,12 +12,10 @@ pub fn terminal_output() -> (fltk::text::TextDisplay, fltk::text::TextBuffer) {
     let terminal_text: String = format!("Operating system: {}\n🦀 Rusty IDE console 💻\n\n", std::env::consts::OS);
     terminal.set_buffer(Some(terminal_buffer.clone())); // Set the text buffer for the text display widget
     terminal.buffer().unwrap().append(&terminal_text); // Add the welcome message to the text buffer
-    terminal.set_scrollbar_align(
-        fltk::enums::Align::Bottom | fltk::enums::Align::Right
-    ); // Set the scrollbar alignment to bottom-right
     terminal.set_text_font(font::FONT);
     terminal.set_text_size(console_text_size::CONSOLE_TEXT_SIZE);
     terminal.set_scrollbar_size(scrollbar_size::SCROLLBAR_SIZE);
+    terminal.set_scrollbar_align(fltk::enums::Align::Right);
     terminal.set_frame(fltk::enums::FrameType::NoBox); // Set the frame type to no box (no visible border)
     (terminal, terminal_buffer)
 }
