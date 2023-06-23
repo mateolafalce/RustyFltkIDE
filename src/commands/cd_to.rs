1fcd2114a31e8fdd1bb88182fa6d7acf961fbe1c
Mateo Lafalce <mateolafalce3@gmail.com>
2023-06-24 00:15:01 +1100

    cd_to.rs

--- src/commands/cd_to.rs
+++ src/commands/cd_to.rs
@@ -1,40 +1,36 @@
-use std::fs::metadata;
-use fltk::{
-    text::{
-        TextDisplay,
-        TextBuffer
-    },
-    dialog::alert
-};
 #[path="../functions/root/set_root.rs"]
 mod set_root;
 #[path="../functions/write_terminal.rs"]
 mod write_terminal;
 #[path="../functions/event/center.rs"]
 mod center;
+#[path="../functions/event/error.rs"]
+mod error;
 
 pub fn cd_to(
     input: String,
-    text: TextBuffer,
-    terminal: TextDisplay,
+    text: fltk::text::TextBuffer,
+    terminal: fltk::text::TextDisplay,
     split_raw_input: Vec<String>,
     root: String
-){
+) -> bool {
     let mut split_string: Vec<&str> = root.split('\\').collect();
-    split_string.pop();
+    split_string.pop();  // Remove the last element (emoji)
     split_string.push(&split_raw_input[1]);
     let new_root: String = split_string.join("\\");
-    match metadata(new_root.clone()) {
+
+    match std::fs::metadata(new_root.clone()) {  // Check if the new root path exists
         Ok(_) => {
             set_root::set_root(new_root.clone()).expect("Error");
             write_terminal::write_terminal(
                 &(root.clone() + " " + &input + "\n"),
                 text.clone(),
                 terminal.clone()
-            );
+            );  // Write the terminal command to the text buffer using the `write_terminal` module
         },
         Err(e) => {
-            alert(center::center().0 - 100, center::center().1 - 100, &format!("Error: {}\n", e));
+            error::error(&e.to_string());  // Display the error message using the `error` module
         }
     }
+    true
 }
