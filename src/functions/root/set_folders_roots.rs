534be5df6b20f1e2e01ac83a6de9c504c1fc45f1
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-05 01:18:53 +1100

    set_folders_roots.rs

--- src/functions/root/set_folders_roots.rs
+++ src/functions/root/set_folders_roots.rs
@@ -1,26 +1,17 @@
-use std::{
-    fs::{
-        File,
-        OpenOptions
-    },
-    io::{
-        Error,
-        Write,
-        Read
-    },
-};
-use fltk::dialog::alert;
+use std::io::{Write,Read};
 #[path="../event/center.rs"]
 mod center;
 #[path="./find_path.rs"]
 mod find_path;
+#[path="../event/error.rs"]
+mod error;
 
 #[allow(dead_code)]
 pub fn set_folders_roots(
     root: String
-) -> Result<(), Error> {
-    // Open the "folders_roots.rs" file with read and write access.
-    let mut file: File = OpenOptions::new().read(true).write(true).open("src/constants/folders_roots.rs").unwrap();
+) -> Result<(), std::io::Error> {
+    // Open the folders_roots.rs file with read and write access.
+    let mut file: std::fs::File = std::fs::OpenOptions::new().read(true).write(true).open("src/constants/folders_roots.rs").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();// Read the current contents of the file into a string.
     if contents.len() == 0 { // If the file is empty, write the new root path to it.
@@ -33,11 +24,7 @@ pub fn set_folders_roots(
             .map(|s| s.to_string())
             .collect();
         if find_path::find_path(&paths, &root) {
-            alert( // If the path is already in the file, show an error message.
-                center::center().0 - 100,
-                center::center().1 - 100,
-                &format!("Error: The path is already in the directory\n")
-            );
+            error::error("Error: The path is already in the directory\n");
         } else {
             file.write_all(&(root + "\\-").as_bytes()).unwrap(); // Otherwise, write the new root path to the file.
         }
