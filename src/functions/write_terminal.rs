ca6c438521b2f3d75c3e3cfdd94d4926a1b1211b
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-08 00:50:46 +1100

    write_terminal.rs

--- src/functions/write_terminal.rs
+++ src/functions/write_terminal.rs
@@ -7,12 +7,15 @@ pub fn write_terminal(
 ) -> bool {
     let mut new_buffer: fltk::text::TextBuffer = text.clone();
     let mut terminal: fltk::text::TextDisplay = terminal;
-    let terminal_text: String = format!("{}{}", text.text(), response); // Combine the current text in the TextBuffer object with the response
-    new_buffer.set_text(&terminal_text); // Set the TextBuffer object to the new combined text
+    // Combine the current text in the TextBuffer object with the response
+    let terminal_text: String = format!("{}{}", text.text(), response);
+    // Set the TextBuffer object to the new combined text
+    new_buffer.set_text(&terminal_text);
     terminal.set_buffer(new_buffer);
+    // Scroll to the end of the text in the TextDisplay object
     terminal.scroll(
         terminal.count_lines(1, terminal.buffer().unwrap().length(),true),
         0
-    ); // Scroll to the end of the text in the TextDisplay object
+    );
     true
 }
