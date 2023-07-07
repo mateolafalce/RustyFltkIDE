82788c0464a9cafd4cfc32e9e1a0e8ebef480331
Mateo Lafalce <mateolafalce3@gmail.com>
2023-07-08 00:46:36 +1100

    set_root.rs

--- src/functions/root/set_root.rs
+++ src/functions/root/set_root.rs
@@ -1,7 +1,9 @@
 use std::io::Write;
 
 pub fn set_root(root: String) -> Result<(), std::io::Error> {
+    // Create a file named "src/constants/root.rs"
     let mut file: std::fs::File = std::fs::File::create("src/constants/root.rs")?;
+    // Write the contents of the 'root' string as bytes to the file
     file.write_all(root.as_bytes())?;
     Ok(())
 }
